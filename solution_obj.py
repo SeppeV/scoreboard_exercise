ec61e5400ee961b758997b21a5a47a82c7b5d86e
SeppeV <seppe.vuerstaek@outlook.com>
2021-06-07 09:13:59 +0200

    9/43 passed

--- solution_obj.py
+++ solution_obj.py
@@ -7,22 +7,18 @@ class Game:
         """Zet de stand op de beginstand bij aanvang van de wedstrijd"""
         # TODO: Gebruik onderstaande member variables,
         # maar corrigeer de initiele waardes:
-        self.points_home = 99
-        self.points_away = 99
-        self.sets_home = 99
-        self.sets_away = 99
-        self.set_nr = 99
+        self.sets_home = 0
+        self.sets_away = 0
+        self.points_home = 0
+        self.points_away = 0
+        self.set_nr = 1
 
     def get_max_points(self):
         """Geef het maximaal aantal punten voor deze set terug."""
         # TODO: Geef de correcte waarde terug afhankelijk van
         # de set_nr.
-        return 99
+        if self.set_nr <= 4:
+            return 25
+        else:
+            return 15
 
-    def score(self, team):
-        """Bereken de nieuwe stand als team `team` gescoord heeft.
-        team: kan "A" of "H" zijn, voor "away" en "home".
-        """
-        # TODO: Pas hier de punten, set punten en set nummer aan.
-        # Gebruik `get_max_points` om te weten bij hoeveel punten je
-        # in de set de set gewonnen hebt.
