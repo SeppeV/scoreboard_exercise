f6afdaac6069fdca0d836c4240b6b30d22510584
SeppeV <seppe.vuerstaek@outlook.com>
2021-06-07 09:30:11 +0200

    43/43 passed

--- solution_obj.py
+++ solution_obj.py
@@ -1,24 +1,36 @@
 class Game:
-    """Deze class houdt de stand bij, en voorziet een functie
-    om de nieuwe stand te berekenen.
-    """
 
     def __init__(self):
-        """Zet de stand op de beginstand bij aanvang van de wedstrijd"""
-        # TODO: Gebruik onderstaande member variables,
-        # maar corrigeer de initiele waardes:
-        self.sets_home = 0
-        self.sets_away = 0
         self.points_home = 0
         self.points_away = 0
+        self.sets_home = 0
+        self.sets_away = 0
         self.set_nr = 1
 
     def get_max_points(self):
-        """Geef het maximaal aantal punten voor deze set terug."""
-        # TODO: Geef de correcte waarde terug afhankelijk van
-        # de set_nr.
-        if self.set_nr <= 4:
+        if self.set_nr < 5:
             return 25
         else:
             return 15
 
+    def score(self, team):
+
+        if team == "H":
+            self.points_home += 1
+
+        if team == "A":
+            self.points_away += 1
+
+        if self.points_home >= self.get_max_points():
+            if self.points_away < self.points_home - 1:
+                self.points_home = 0
+                self.points_away = 0
+                self.sets_home += 1
+                self.set_nr += 1
+
+        if self.points_away >= self.get_max_points():
+            if self.points_home < self.points_away - 1:
+                self.points_home = 0
+                self.points_away = 0
+                self.sets_away += 1
+                self.set_nr += 1
